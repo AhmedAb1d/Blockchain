a2a742bf60060e7a78d278931fe1e0215578612e
FirasHadjAmeur <99393357+FirasHadjAmeur@users.noreply.github.com>
2023-01-23 15:08:58 +0100

    Port change

--- backend/Server.py
+++ backend/Server.py
@@ -47,4 +47,4 @@ def new_transaction():
         blockchain.mine()
     return "Transaction added to the block"
 
-app.run(debug=True, port=5000, host="0.0.0.0")
+app.run(debug=True, port=5010, host="0.0.0.0")
